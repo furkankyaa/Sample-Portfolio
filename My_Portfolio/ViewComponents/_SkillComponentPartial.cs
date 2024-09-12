cab85d82b58e8d27de9a0301d06281e83e4f0f6d
Furkan <furkannkayaa90@gmail.com>
2024-09-12 16:01:39 +0300

    eksiklikler

---
+++ My_Portfolio/ViewComponents/_SkillComponentPartial.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using My_Portfolio.DAL.Context;
+using My_Portfolio.DAL.Entities;
+
+namespace My_Portfolio.ViewComponents
+{
+    public class _SkillComponentPartial:ViewComponent
+    {
+        MyPortfolioContext context =new MyPortfolioContext();
+
+        
+        public IViewComponentResult Invoke() {
+            
+            var values = context.Skills.ToList();
+            
+            
+            return View(values); }
+    }
+}
